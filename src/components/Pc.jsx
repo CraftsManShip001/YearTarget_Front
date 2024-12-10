4ed62b6c44be1312c1d212ce0d6d623027240f6b
서정현 <150584831+CraftsManShip001@users.noreply.github.com>
2024-12-11 02:06:29 +0900

    YearTarget 0.06

--- src/components/Pc.jsx
+++ src/components/Pc.jsx
@@ -1,12 +1,62 @@
-import pc from '../assets/pc.png';
+import '../styles/Pc.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : (time);
+}
+
+
 
 function Pc(){
+    let [days,setDays] = useState(0);
+    let [hours,setHours] = useState(0);
+    let [mins,setMins] = useState(0);
+    let [seconds,setSeconds] = useState(0);
+
+    useEffect(() => {
+        const updateCountdown = () => {
+            const newYear = new Date(2025, 0, 1);
+            const current = new Date();
+
+            const diffTime = Math.abs(newYear - current);
+
+            setDays(formatTime(Math.floor(diffTime / (1000 * 3600 * 24))));
+            setHours(formatTime(Math.floor((diffTime / (1000 * 3600)) % 24)));
+            setMins(formatTime(Math.floor((diffTime / (1000 * 60)) % 60)));
+            setSeconds(formatTime(Math.floor((diffTime / 1000) % 60)));
+        };
+        updateCountdown();
+        const intervalId = setInterval(updateCountdown, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
+    const preventScroll = (event) => {
+        event.preventDefault();
+    };
+    document.body.addEventListener('touchmove', preventScroll, { passive: false });
     return(
-        <div className='container'>
-            <div className="maincontainer">
-                <div className="textcontainer">
-                    <h1 style = {{fontSize:'5rem'}}className='YearTarget tracking-in-expand'>{'니어 타겟\n'}</h1>
-                    <h1 style = {{fontSize:'3rem'}}className='YearTarget text-focus-in'>{'모바일로 접속해주세요!\n'}</h1>
+        <div className='scroll'>
+            <div className='container'>
+                <div className="maincontainer">
+                        <div className="timertext" style={{marginLeft:"12%"}}>
+                            <p className="timer" id="day">{days}</p>
+                            <span>days</span>
+                        </div>
+
+                        <div className="timertext">
+                            <p className="timer" id="hours">{hours}</p>
+                            <span>hours</span>
+                        </div>
+
+                        <div className="timertext">
+                            <p className="timer" id="mins">{mins}</p>
+                            <span>mins</span>
+                        </div>
+
+                        <div className="timertext">
+                            <p className="timer" id="seconds">{seconds}</p>
+                            <span>seconds</span>
+                        </div>
                 </div>
             </div>
         </div>
